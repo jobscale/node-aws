219ede2b5529e1fe56571a9343db580a9156c9a2
jobscale <jobscale@users.noreply.github.com>
2024-03-08 00:01:55 +0900

    daemon-test.sh updated

--- daemon-test.sh
+++ daemon-test.sh
@@ -1,10 +1,15 @@
-while true
-do
-  echo restart
-  docker restart dind
-  sleep 3.3
-  docker exec -it dind docker ps -a
-  echo sleep
-  sleep 2.2
-done
+#!/usr/bin/env bash
 
+{
+  docker rm -f dind
+  docker run --name dind --privileged -it ghcr.io/jobscale/node-aws:dind bash
+  while true
+  do
+    echo "restart $(docker restart dind)"
+    sleep 3.3
+    echo exec
+    docker exec -it dind docker ps -a
+    echo -e "\nsleep"
+    sleep 2.2
+  done
+}
